d99fdcda41407d0f134b35aef04a9ae6bbbe0179
holdonL <pavelshets@gmail.com>
2018-09-19 02:31:56 +0300

    добавил вторую кнопку для перехода на карту

--- map.js
+++ map.js
@@ -1,8 +1,14 @@
 var button = document.querySelector(".js-show-map");
+var button2 = document.querySelector(".js-show-map2");
 var map = document.querySelector(".modal-map");
 var closemap = map.querySelector(".modal-close");
 
     button.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        map.classList.add("modal-show");
+})
+
+    button2.addEventListener("click", function(evt) {
         evt.preventDefault();
         map.classList.add("modal-show");
 })
